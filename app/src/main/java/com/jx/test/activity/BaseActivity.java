b993e615eb4ab4720f2461a225411969e9b0e30c
Rose-cwy <963375122@qq.com>
2017-12-05 15:57:24 +0800

    BaseActivity

---
+++ app/src/main/java/com/jx/test/activity/BaseActivity.java
@@ -0,0 +1,76 @@
+package com.jx.test.activity;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * Created by c on 2017/12/5.
+ */
+
+
+public abstract class BaseActivity extends SupportActivity {
+
+    protected Activity mContext;
+    private Unbinder mUnBinder;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getRootView());
+
+        mContext = this;
+        //绑定ButterKnife
+        mUnBinder = ButterKnife.bind(this);
+        Log.e("BaseActivity", this.getClass().getSimpleName());
+        setTranslucentStatus(true);
+        init();
+    }
+
+    //加载布局
+    protected abstract int getRootView();
+    //初始化控件
+    protected abstract void init();
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("onDestroy",  this.getClass().getName() + "------>onDestroy");
+        //解除绑定
+        if (mUnBinder != null)
+            mUnBinder.unbind();
+    }
+
+    /**
+     * 设置沉浸式
+     *
+     * @param on
+     */
+    protected void setTranslucentStatus(boolean on) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window win = getWindow();
+            WindowManager.LayoutParams winParams = win.getAttributes();
+            final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            if (on) {
+                winParams.flags |= bits;
+            } else {
+                winParams.flags &= ~bits;
+            }
+            win.setAttributes(winParams);
+        }
+    }
+
+
+
+
+
+}
