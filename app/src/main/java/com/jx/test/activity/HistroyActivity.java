983f31d3b0952c4073fdff8371c18ebd3fa0fddd
Administrator <1053864253@qq.com>
2017-12-11 08:41:57 +0800

    王一鸣卡片修改

--- app/src/main/java/com/jx/test/activity/HistroyActivity.java
+++ app/src/main/java/com/jx/test/activity/HistroyActivity.java
@@ -1,16 +1,23 @@
 package com.jx.test.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.jx.test.R;
+import com.jx.test.detail.ShiPActivity;
 import com.jx.test.find.adapter.HistoryGridAdapter;
 import com.jx.test.find.greendao.HistroyBean;
 import com.jx.test.find.greendao.gen.DaoMaster;
 import com.jx.test.find.greendao.gen.DaoSession;
 import com.jx.test.find.greendao.gen.HistroyBeanDao;
+import com.jx.test.sift.bean.MyDataId;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.List;
 
@@ -54,6 +61,16 @@ public class HistroyActivity extends AppCompatActivity {
 
         adapter = new HistoryGridAdapter(list,HistroyActivity.this);
         gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                EventBus.getDefault().postSticky(new MyDataId(list.get(i).getMovieid()));
+
+                Intent intent = new Intent(HistroyActivity.this, ShiPActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
